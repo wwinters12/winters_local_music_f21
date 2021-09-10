184ed72ff0569820d6449d1f927047219ac69b19
Billy Winters <wwinters@ithaca.edu>
2021-09-10 12:33:31 -0400

    lab 3 progress

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import NewArtistForm
 
 
 @app.route('/')
@@ -13,10 +14,16 @@ def artists():
     return render_template('artists.html')
 
 
-@app.route('/new_artists')
-def new_artists():
-    return render_template('new_artists.html')
-
 @app.route('/artist')
 def artist():
     return render_template('artist.html')
+
+
+@app.route('/new_artists', methods=['GET', 'POST'])
+def new_artists():
+    form = NewArtistForm()
+    if form.validate_on_submit():
+        flash('New artist created: {}'.format(
+            form.artist.data))
+        return redirect(url_for('new_artists'))
+    return render_template('new_artists.html', title='Create a New Artist', form=form)
