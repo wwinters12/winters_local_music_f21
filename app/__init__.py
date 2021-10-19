7dd4507aa2222d37ee73de248cffb9bff00afe88
Billy Winters <wwinters@ithaca.edu>
2021-10-19 15:45:00 -0400

    lab 5 progress

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from app import routes, models
 
 app = Flask(__name__)
 app.config.from_object(Config)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect, url_for
-from wtforms.ext.appengine import db
-from app import app
+from app import app, db
 from app.forms import NewArtistForm
 from app.models import Artist, Event, ArtistToEvent, Venue
 
@@ -48,7 +47,7 @@ def new_artists():
     return render_template('new_artists.html', title='Create a New Artist', form=form)
 
 
-@app.route("/populate_db")
+@app.route('/populate_db')
 def populate_db():
     clear_db()
 
@@ -64,10 +63,18 @@ def populate_db():
     db.session.add_all([a1, a2, a3])
     db.session.commit()
 
-    e1 = Event(eventname="Hey.", date='1')
-    e2 = Event(eventname="Hey there.", date='1')
-    e3 = Event(eventname="Hey.", date='1')
-    db.session.add_all([e1, e2, e3])
+    e1 = Event(eventname="Coachella", date='10/30/21')
+    e2 = Event(eventname="Lalapalooza", date='11/7/21')
+    e3 = Event(eventname="Octoberfest", date='10/6/22')
+    e4 = Event(eventname="Governors Ball", date='12/15/21')
+    e5 = Event(eventname="Woodstock", date='12/26/21')
+    db.session.add_all([e1, e2, e3, e4, e5])
+    db.session.commit()
+
+    v1 = Venue(eventname="Madison Square Garden", date='10/30')
+    v2 = Venue(eventname="The Greek Theatre", date='11/7')
+    v3 = Venue(eventname="The Dock", date='11/6')
+    db.session.add_all([v1, v2, v3])
     db.session.commit()
     flash("Database has been populated")
     return render_template('base.html', title='Home')
