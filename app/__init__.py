1a827bb8ca141a9355323f8d000fd4d8bf199a70
Billy Winters <wwinters@ithaca.edu>
2021-10-19 15:30:09 -0400

    lab 5 progress

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from app import routes, models
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-
-from app import routes
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template, flash, redirect, url_for
+from wtforms.ext.appengine import db
 from app import app
 from app.forms import NewArtistForm
+from app.models import Artist, Event, ArtistToEvent, Venue
 
 
 @app.route('/')
@@ -11,12 +13,29 @@ def index():
 
 @app.route('/artists')
 def artists():
-    return render_template('artists.html')
+    artist_list = {'John Browns Body',
+                   'The Gun Poets',
+                   'Donna The Buffalo',
+                   'The Blind Spots'}
+    return render_template('artists.html', artist_list=artist_list)
 
 
 @app.route('/artist')
 def artist():
-    return render_template('artist.html')
+    info = [
+        {
+            'artist': 'The Gun Poets',
+            'hometown': 'Beautiful day in Portland!',
+            'description': 'Voice as a weapon, words as bullets, spreading the universal message of peace, love, '
+                           'and justice through music. Sure, theres a cynical cultural tendency to make certain '
+                           'assumptions when you hear the word "gun" associated with rap music, but this seven-member '
+                           'live hip-hop band from Ithaca, NY, runs contrary to that image with their positive '
+                           'message and uplifting performances. ',
+            'events': {'The Commons on Thursday 9/6',
+                       'The Haunt next Friday 9/14'}
+        }
+    ]
+    return render_template('artist.html', info=info)
 
 
 @app.route('/new_artists', methods=['GET', 'POST'])
@@ -27,3 +46,38 @@ def new_artists():
             form.artist.data))
         return redirect(url_for('new_artists'))
     return render_template('new_artists.html', title='Create a New Artist', form=form)
+
+
+@app.route("/populate_db")
+def populate_db():
+    clear_db()
+
+    a1 = Artist(artistname='The Gun Poets',
+                hometown='Ithaca',
+                description='Ithaca Hip Hop Band')
+    a2 = Artist(artistname='Stone Cold Miracle',
+                hometown='Fall Creek',
+                description='Funky Soul')
+    a3 = Artist(artistname='The New Team',
+                hometown='San Francisco',
+                description='They play new-age music. It sux.')
+    db.session.add_all([a1, a2, a3])
+    db.session.commit()
+
+    e1 = Event(eventname="Hey.", date='1')
+    e2 = Event(eventname="Hey there.", date='1')
+    e3 = Event(eventname="Hey.", date='1')
+    db.session.add_all([e1, e2, e3])
+    db.session.commit()
+    flash("Database has been populated")
+    return render_template('base.html', title='Home')
+
+
+def clear_db():
+    flash("Resetting database: deleting old data and repopulating with dummy data")
+    # clear all data from all tables
+    meta = db.metadata
+    for table in reversed(meta.sorted_tables):
+        print('Clear table {}'.format(table))
+        db.session.execute(table.delete())
+    db.session.commit()
